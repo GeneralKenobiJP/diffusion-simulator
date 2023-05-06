f094391e0c6eb20dd1c388027972c253221e7933
GeneralKenobiJP <kubapietrzak7@gmail.com>
2023-05-06 17:09:06 +0200

    StandardBehaviour 2

--- Diffusion Simulator/Assets/Scripts/PhysicalConst.cs
+++ Diffusion Simulator/Assets/Scripts/PhysicalConst.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-
 public static class PhysicalConst
 {
     public const double BOLTZMANN=1.380649e-23;
--- Diffusion Simulator/Assets/Scripts/StandardBehaviour.cs
+++ Diffusion Simulator/Assets/Scripts/StandardBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class StandardBehaviour : MonoBehaviour
 {
@@ -9,30 +10,31 @@ public class StandardBehaviour : MonoBehaviour
     private double velocity;
     public const float mass=32f;
     //Random rnd = new static Random();
-    private const double sigma=1f/*=PhysicalConst.BOLTZMANN*/; //standard deviation
-    private const double mi=1f; //mean
+    private double sigma; //standard deviation
+    private double mi=0f; //mean
 
     private void SetVelocityStd()
     {
         //Box-Muller Transform
-        //var rnd = new Random();
-        //Random rnd = new Random();
-        var u1=1f;
-        var u2=4f;
+        var u1=UnityEngine.Random.Range(0.001f,1);
+        var u2=UnityEngine.Random.Range(0.001f,1);
         var z=Mathf.Sqrt(-2*Mathf.Log(u1))*Mathf.Cos(2*Mathf.PI*u2);
         velocity = z*sigma+mi;
+        Debug.Log(velocity);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sigma = Math.Sqrt(PhysicalConst.BOLTZMANN*temperature/mass);
+        //sigma*=1e+11;
+        //sigma=1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetVelocityStd();
     }
 
     void FixedUpdate()
