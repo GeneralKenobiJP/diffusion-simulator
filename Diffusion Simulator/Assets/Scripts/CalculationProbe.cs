94f92542d5d4c6bc4ec379fcae87dad2aa673436
Jakub Pietrzak <kubapietrzak7@gmail.com>
2023-05-29 22:34:30 +0200

    Calculation Probe 2
    Added particle recognition script for calculation probes

--- Diffusion Simulator/Assets/Scripts/CalculationProbe.cs
+++ Diffusion Simulator/Assets/Scripts/CalculationProbe.cs
@@ -6,6 +6,8 @@ public class CalculationProbe : MonoBehaviour
 {
     private List<GameObject> probeList;
     public float probeRadius;
+    public List<ParticleType> substances;
+    private float[] massCenter; //array of centers of the masses of physical systems consisting of particular particle type (substance)
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +24,40 @@ public class CalculationProbe : MonoBehaviour
     {
         Collider[] colliderArray;
         colliderArray = Physics.OverlapSphere(this.transform.position,probeRadius);
+        if(colliderArray.Length>0)
+        {
+            foreach(var item in colliderArray)
+            {
+                if(item.tag=="Particle")
+                {
+                    var i = GetSubstanceNum(item);
+                    //Debug.Log(i);
+                }
+            }
+        }
+    }
+
+    void InitializeMassCenter()
+    {
+
+    }
+
+    private int GetSubstanceNum(Collider collider)
+    {
+        var i=0;
+        foreach(var item in substances)
+        {
+            //Debug.Log(item.type);
+            //Debug.Log(collider.GetComponent<StandardBehaviour>().particleType.type);
+            if(item.type==collider.GetComponent<StandardBehaviour>().particleType.type)
+                return i;
+            i++;
+        }
+        return 0; //emergency return
+    }
+
+    void AddCohesion()
+    {
+
     }
 }
--- Diffusion Simulator/Assets/Scripts/Setup.cs
+++ Diffusion Simulator/Assets/Scripts/Setup.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class Setup : MonoBehaviour
 {
-    private const int NUM_PARTICLES = 500;
+    private const int NUM_PARTICLES = 400;
+    private int[] numParticles={250,250};
+    public List<ParticleType> substanceArray;
     public GameObject Particle;
     public GameObject CalculationProbeObject;
     private StandardBehaviour particleScript;
     public float temperature;
-    private string particleType="chlorine"; //private as for now
+    public List<string> particleType;
 
     private const int Y_PROBE_PRECISION=6;
     private const int ANG_PROBE_PRECISION=4;
     private const int R_PROBE_PRECISION=2;
+    private /*const*/ float PROBE_RADIUS_MINIMUM;
 
     // Start is called before the first frame update
     void Start()
     {
+        var a = "water";
+        var b = "chlorine";
+        particleType.Add(a);
+        particleType.Add(b);
+        LoadSubstances();
         for(var i=0;i<NUM_PARTICLES;i++)
         {
             Instantiate(Particle);
+            Particle.tag="Particle";
             particleScript = Particle.GetComponent<StandardBehaviour>();
             particleScript.temperature=temperature;
-            particleScript.particleType=jsonSerializer.SearchForParticle(particleType);
+            AssignSubstances(particleScript, i);
+            //particleScript.particleType=jsonSerializer.SearchForParticle(particleType);
             /*Debug.Log(particleScript.particleType.boilingPoint);
             Debug.Log(particleScript.particleType.type);
             Debug.Log(particleScript.particleType.color[0]);
@@ -32,6 +42,7 @@ public class Setup : MonoBehaviour
         }
 
         CylinderScatter(CalculationProbeObject);
+
     }
 
     // Update is called once per frame
@@ -49,7 +60,9 @@ public class Setup : MonoBehaviour
 
         var posY=center.y;
 
-        var probeRadius = Mathf.Max(2f*height/Y_PROBE_PRECISION,radius/R_PROBE_PRECISION,2f*radius/R_PROBE_PRECISION*Mathf.Sin(2f*Mathf.PI/ANG_PROBE_PRECISION)/*distance between two points sharing r and having different angle*/);
+        var probeRadius = 0f;
+        PROBE_RADIUS_MINIMUM = Mathf.Max(2f*height/Y_PROBE_PRECISION,radius/R_PROBE_PRECISION);
+        //var probeRadius = Mathf.Max(2f*height/Y_PROBE_PRECISION,radius/R_PROBE_PRECISION,2f*radius/R_PROBE_PRECISION*Mathf.Sin(2f*Mathf.PI/ANG_PROBE_PRECISION)/*distance between two points sharing r and having different angle*/);
         Debug.Log(2f*height/Y_PROBE_PRECISION);
         Debug.Log(radius/R_PROBE_PRECISION);
         Debug.Log(2f*radius/R_PROBE_PRECISION*Mathf.Sin(2f*Mathf.PI/ANG_PROBE_PRECISION));
@@ -73,14 +86,54 @@ public class Setup : MonoBehaviour
                 {
                     var obInst = Instantiate(obj);
                     obInst.transform.position = new Vector3(center.x+r*Mathf.Cos(ang),posY,center.z+r*Mathf.Sin(ang));
+                    probeRadius = Mathf.Max(PROBE_RADIUS_MINIMUM,2f*radius/R_PROBE_PRECISION*Mathf.Sin(2f*Mathf.PI/ANG_PROBE_PRECISION)/*distance between two points sharing r and having different angle*/);
+                    Debug.Log(probeRadius);
                     obInst.GetComponent<CalculationProbe>().probeRadius=probeRadius;
-                    //obInst.AddComponent<SphereCollider>();
-                    //obInst.GetComponent<SphereCollider>().radius=probeRadius;
-                    Debug.Log(obInst.GetComponent<CalculationProbe>().probeRadius);
+                    obInst.GetComponent<CalculationProbe>().substances=substanceArray;
+                    //Debug.Log(obInst.GetComponent<CalculationProbe>().substances[0].type);
+                    //Debug.Log(obInst.GetComponent<CalculationProbe>().substances[1].type);
                 }
             }
             //var obInstant = Instantiate(obj);
             //obInstant.transform.position = new Vector3(center.x,posY,center.z);
         }
+    }
+
+    public void LoadSubstances()
+    {
+        var i=0;
+        Debug.Log(particleType);
+        foreach(string item in particleType)
+        {
+            Debug.Log("srutututu: " + i);
+            i++;
+            Debug.Log(item);
+            Debug.Log(jsonSerializer.SearchForParticle(item).type);
+            //var x = jsonSerializer.SearchForParticle(item);
+            substanceArray.Add(jsonSerializer.SearchForParticle(item));
+        }
+    }
+
+    public void AssignSubstances(StandardBehaviour script, int num)
+    {
+        /*foreach(var item in particleType)
+        {
+            Debug.Log(item);
+            Debug.Log(jsonSerializer.SearchForParticle(item).type);
+            //var x = jsonSerializer.SearchForParticle(item);
+            substanceArray.Add(jsonSerializer.SearchForParticle(item));
+        }*/
+        var j=0;
+        while(num>0)
+        {
+            if(num>numParticles[j])
+                num-=numParticles[j];
+            else
+            {
+                break;
+            }
+            j++;
+        }
+        script.particleType = substanceArray[j];
     }
 }
