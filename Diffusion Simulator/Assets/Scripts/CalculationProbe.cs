a91db14c019236370d5a094db1c3cab5204bc743
Jakub Pietrzak <kubapietrzak7@gmail.com>
2023-06-02 20:24:48 +0200

    AddCohesion()
    AddCohesion function was created (and functions associated with it); ApplyGravity() was created, but was not debugged nor used

--- Diffusion Simulator/Assets/Scripts/CalculationProbe.cs
+++ Diffusion Simulator/Assets/Scripts/CalculationProbe.cs
@@ -9,14 +9,34 @@ public class CalculationProbe : MonoBehaviour
     public float probeRadius;
     public List<ParticleType> substances;
     public List<StandardBehaviour> colliderScriptList;
+    public List<List<StandardBehaviour>> colliderScriptListSortedForSubstances;
     private float pressureForce; //positive for up, negative for down
-    private float[] massCenter; //array of centers of the masses of physical systems consisting of particular particle type (substance)
+    private Vector3[] massCenter; //array of centers of the masses of physical systems consisting of particular particle type (substance)
     // Start is called before the first frame update
     void Start()
     {
         pressureForce=0f;
+        SetupColliderScriptListSorted();
         Probe();
         StartCoroutine(Compute());
+    }
+
+    private void SetupColliderScriptListSorted()
+    {
+        colliderScriptListSortedForSubstances = new List<List<StandardBehaviour>>();
+        foreach(var item in substances)
+            colliderScriptListSortedForSubstances.Add(new List<StandardBehaviour>());
+        massCenter = new Vector3[substances.Count];
+        for(var i=0;i<substances.Count;i++)
+        {
+            massCenter[i] = new Vector3(0f,0f,0f);
+        }
+    }
+    private void CleanColliderScriptListSorted()
+    {
+        for(var i=0;i<substances.Count;i++)
+            colliderScriptListSortedForSubstances[i] = new List<StandardBehaviour>();
+
     }
 
     // Update is called once per frame
@@ -33,7 +53,8 @@ public class CalculationProbe : MonoBehaviour
         while(true)
         {
             Probe();
-            AddPressure();
+            ApplyPressure();
+            ApplyCohesion();
             //Debug.Log("Compute");
             yield return timeDelay;
         }
@@ -44,18 +65,25 @@ public class CalculationProbe : MonoBehaviour
         Collider[] colliderArray;
         colliderArray = Physics.OverlapSphere(this.transform.position,probeRadius);
         colliderScriptList = new List<StandardBehaviour>();
+        var localMass = new float[substances.Count];
+        CleanColliderScriptListSorted();
         if(colliderArray.Length>0)
         {
             foreach(var item in colliderArray)
             {
                 if(item.tag=="Particle")
                 {
-                    colliderScriptList.Add(item.GetComponent<StandardBehaviour>());
+                    var itemStandardBehaviour = item.GetComponent<StandardBehaviour>();
+                    colliderScriptList.Add(itemStandardBehaviour);
                     var i = GetSubstanceNum(item);
                     //Debug.Log(i);
+                    colliderScriptListSortedForSubstances[i].Add(itemStandardBehaviour);
+                    AddToMass(item,i,ref localMass[i]);
                 }
             }
-            ApplyPressure();
+            AddPressure();
+            FinalizeMass(localMass);
+            AddCohesion();
         }
     }
 
@@ -77,12 +105,24 @@ public class CalculationProbe : MonoBehaviour
         }
         return 0; //emergency return
     }
-
+    ///ADD FORCES (used to calculate forces before applying them)
+    private void AddToMass(Collider thisParticle, int substanceNum, ref float localMass)
+    {
+        var thisMass = thisParticle.GetComponent<StandardBehaviour>().mass;
+        massCenter[substanceNum] = thisParticle.transform.position*thisMass;
+        localMass+=thisMass;
+    }
+    private void FinalizeMass(float[] localMass)
+    {
+        for(var i=0;i<substances.Count;i++)
+        {
+            massCenter[i]/=localMass[i];
+        }
+    }
     private void AddCohesion()
     {
 
     }
-
     private void AddPressure()
     {
         var s=0f;
@@ -108,7 +148,7 @@ public class CalculationProbe : MonoBehaviour
         }
         return s;
     }
-
+    ///APPLY FORCES (Used to influence particles)
     private void ApplyPressure()
     {
         var pressureVector = new Vector3(0,1f,0);
@@ -119,5 +159,19 @@ public class CalculationProbe : MonoBehaviour
         {
             item.ApplyForce(pressureVector);
         }
+    }
+    private void ApplyGravity()
+    {
+        var gravityVector = new Vector3(0,-0.1f,0);
+        foreach(var item in colliderScriptList)
+        {
+            gravityVector *= item.mass;
+            item.ApplyForce(gravityVector);
+        }
+    }
+
+    private void ApplyCohesion()
+    {
+        
     }
 }
